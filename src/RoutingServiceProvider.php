4cef855abf0b5ce3931b64b465f4113c1e4806fd
Frano Sasvari <frane.sasvari@gmail.com>
2017-07-27 13:12:57 +0000

    Apply fixes from StyleCI

--- src/RoutingServiceProvider.php
+++ src/RoutingServiceProvider.php
@@ -3,7 +3,6 @@
 namespace LaravelTrailingSlash;
 
 use Illuminate\Routing\RoutingServiceProvider as BaseRoutingServiceProvider;
-use LaravelTrailingSlash\UrlGenerator;
 
 class RoutingServiceProvider extends BaseRoutingServiceProvider
 {
@@ -14,30 +13,30 @@ class RoutingServiceProvider extends BaseRoutingServiceProvider
      */
     public function register()
     {
-		$this->app->singleton('url', function($app) {
-			$routes = $app['router']->getRoutes();
+        $this->app->singleton('url', function ($app) {
+            $routes = $app['router']->getRoutes();
 
-			// The URL generator needs the route collection that exists on the router.
+            // The URL generator needs the route collection that exists on the router.
             // Keep in mind this is an object, so we're passing by references here
             // and all the registered routes will be available to the generator.
-			$app->instance('routes', $routes);
+            $app->instance('routes', $routes);
 
-			$url = new UrlGenerator(
-				$routes, $app->rebinding('request', $this->requestRebinder())
-			);
+            $url = new UrlGenerator(
+                $routes, $app->rebinding('request', $this->requestRebinder())
+            );
 
-			$url->setSessionResolver(function ($app) {
-				return $app['session'];
-			});
+            $url->setSessionResolver(function ($app) {
+                return $app['session'];
+            });
 
-			// If the route collection is "rebound", for example, when the routes stay
+            // If the route collection is "rebound", for example, when the routes stay
             // cached for the application, we will need to rebind the routes on the
             // URL generator instance so it has the latest version of the routes.
-			$app->rebinding('routes', function ($app, $routes) {
+            $app->rebinding('routes', function ($app, $routes) {
                 $app['url']->setRoutes($routes);
             });
 
-			return $url;
-		});
+            return $url;
+        });
     }
 }
--- src/UrlGenerator.php
+++ src/UrlGenerator.php
@@ -6,28 +6,30 @@ use Illuminate\Routing\UrlGenerator as BaseUrlGenerator;
 
 class UrlGenerator extends BaseUrlGenerator
 {
-	/**
+    /**
      * Format the given URL segments into a single URL.
      *
-     * @param  string  $root
-     * @param  string  $path
-     * @param  string  $tail
+     * @param string $root
+     * @param string $path
+     * @param string $tail
+     *
      * @return string
      */
-	protected function trimUrl($root, $path, $tail = '')
-	{
-		return parent::trimUrl($root, $path, $tail).(str_contains($path, '#') ? '' : '/');
-	}
+    protected function trimUrl($root, $path, $tail = '')
+    {
+        return parent::trimUrl($root, $path, $tail).(str_contains($path, '#') ? '' : '/');
+    }
 
-	/**
+    /**
      * Format the given URL segments into a single URL.
      *
-     * @param  string  $root
-     * @param  string  $path
+     * @param string $root
+     * @param string $path
+     *
      * @return string
      */
     public function format($root, $path)
-	{
-		return parent::format($root, $path).(str_contains($path, '#') ? '' : '/');
-	}
+    {
+        return parent::format($root, $path).(str_contains($path, '#') ? '' : '/');
+    }
 }
